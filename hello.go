fec7737f3bc148da5d6732b3d2eb2b123bcffbe6
Kunzhou Yang <ykz_ywzq@163.com>
2025-04-18 16:36:46 +0800

    feat: 新增了当前版本号显示

--- hello.go
+++ hello.go
@@ -2,6 +2,10 @@ package hello
 
 import "fmt"
 
-func SayHi(name string){
-  fmt.Printf("你好%s，我是yangkunzhou。很高兴认识你~\n", name)
+func SayHi(name string) {
+	fmt.Printf("你好%s，我是yangkunzhou。很高兴认识你~\n", name)
+}
+
+func ShowVersion() {
+	fmt.Printf("当前版本号：V%s", "0.0.2")
 }
